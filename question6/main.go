84b83c94731ad4e2679b0321ebf7b5729b4aef7d
wangxu <wxmygit@protonmail.com>
2024-07-25 09:23:11 +0800

    message question6

--- question6/main.go
+++ question6/main.go
@@ -3,31 +3,33 @@ package main
 
 import "fmt"
 
-//用于根据角标删除数组元素的函数
-func del_num(num []int, index int) {
-	num1 := num[index:]
-	num = num[:index-1]
-	num = append(num, num1...)
+// //用于查找两个数组的交集函数,利用哈希表
+func intersect(nums1 []int, nums2 []int) []int {
+	result := []int{}                    //创建一个存储结果的数集
+	len1, len2 := len(nums1), len(nums2) //获取两数组的长度
+	longlen, shortlen := len1, len2      //先赋值长度(默认第一个数组长)
+	longnum, shortnum := nums1, nums2    //再赋值数组
+	if len1 < len2 {                     //如果长度是后面的常就反过来赋值
+		longlen, shortlen = len2, len1
+		longnum, shortnum = nums2, nums1
+	}
+	myhashtable := make(map[int]int) //创建哈希表
+	for i := 0; i < shortlen; i++ {
+		myhashtable[shortnum[i]] += 1 //对短的数组进行哈希计数
+	}
+	for i := 0; i < longlen; i++ {
+		for key, value := range myhashtable { //用长的数组和哈希表中的内容逐一去做对比
+			if key == longnum[i] && value > 0 { //如果出现相等的情况并且哈希表的值是大于零的情况
+				result = append(result, key) //直接写入结果数组
+				myhashtable[key]--           //修改哈希数组的值
+			}
+		}
+	}
+	return result //返回结果集
 }
 
-// //用于查找两个数组的交集函数
-// func intersect(nums1 []int, nums2 []int) []int {
-// 	//首先获取到两个数组的长度,初始化两个数组的指针
-// 	n1, n2, p1, p2, long, short := len(nums1), len(nums2), 0, 0, 0, 0
-// 	var num3 []int
-// 	if n1 > n2 { //先判断哪个短然后就用哪个作为结束长度
-// 		long = n1
-// 		short = n2
-// 	} else {
-// 		long = n1
-// 		short = n2
-// 	}
-// 	for p2 < short { //利用short进行对比
-
-// 	}
-// }
 func main() {
-	nums := []int{1, 2, 3, 4}
-	del_num(nums, 2)
-	fmt.Println(nums)
+	nums1, nums2 := []int{4, 9, 5}, []int{9, 4, 9, 8, 4} //创建用于测试的数组
+	result := intersect(nums1, nums2)                    //调用测试
+	fmt.Println(result)                                  //输出结果
 }
